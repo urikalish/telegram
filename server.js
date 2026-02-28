72158289944c0d583413a51f2d35bc01bf9eff76
kalish <ukalish@opentext.com>
2026-02-28 13:47:47 +0200

    wip

--- server.js
+++ server.js
@@ -8,26 +8,33 @@ const CHAT_ID = process.env.CHAT_ID;
 if (!BOT_TOKEN) throw new Error('Missing essential environment variable: BOT_TOKEN');
 if (!CHAT_ID) throw new Error('Missing essential environment variable: CHAT_ID');
 
+const ROOT_COMMANDS = `/help - lists all available commands.
+/time - lists current time on bot server.
+/deni - watch Deni Avdija's status.`;
+
 const bot = new Telegraf(BOT_TOKEN);
 
-const AVAILABLE_COMMANDS = `/help - lists all available commands.
-/time - lists current time on bot server.
-/avdija - lists Deni Avdija's status.`;
+let deniStatusWatchTimerId = 0;
+let lastReportedDeniStatus = '';
 
 async function postMessage(message) {
     try {
         await bot.telegram.sendMessage(CHAT_ID, message);
-        console.log('Message sent successfully!');
+        console.log(`Message sent successfully`);
     } catch (error) {
-        console.error('Error sending message:', error);
+        console.error(`Error sending message:`, error);
     }
 }
 
 bot.command('help', (ctx) => {
-    ctx.reply(AVAILABLE_COMMANDS);
+    console.log(`Running help command`);
+    const result = ROOT_COMMANDS;
+    console.log(result);
+    ctx.reply(result);
 });
 
 bot.command('time', (ctx) => {
+    console.log(`Running time command`);
     const now = new Date();
     const options = {
         day: '2-digit', month: '2-digit', year: '2-digit',
@@ -35,11 +42,13 @@ bot.command('time', (ctx) => {
         hour12: false
     };
     const timeStr = new Intl.DateTimeFormat('en-GB', options).format(now);
-    ctx.reply(`Current time on bot server: ${timeStr}`);
+    const result = `Current time on bot server: ${timeStr}`;
+    console.log(result);
+    ctx.reply(result);
 });
 
-bot.command('avdija', async (ctx) => {
-    console.log(`Fetching Avdija's status...`);
+async function fetchDeniStatus() {
+    console.log(`Fetching Deni Avdija's status...`);
     let status = '';
     try {
         const url = `https://www.espn.com/nba/player/_/id/4683021/deni-avdija`;
@@ -54,15 +63,55 @@ bot.command('avdija', async (ctx) => {
     } catch (error) {
         console.error(error);
     }
-    const result = status ? `Avdija's status: ${status}` : `Unable to get Avdija's status`;
+    const result = status ? `Deni's status: ${status}` : `Unable to get Deni's status`;
     console.log(result);
+    return result;
+}
+
+async function watchDeniStatus(ctx) {
+    const result = await fetchDeniStatus();
+    if (result !== lastReportedDeniStatus) {
+        ctx.reply(result);
+        lastReportedDeniStatus = result;
+    }
+    deniStatusWatchTimerId = setTimeout(() => {
+        watchDeniStatus(ctx).catch(console.error);
+    }, 5 * 60 * 1000);
+}
+
+bot.command('status', async (ctx) => {
+    console.log(`Running status command`);
+    const result = await fetchDeniStatus();
     ctx.reply(result);
+});
+
+bot.command('start', async (ctx) => {
+    console.log(`Running start command`);
+    ctx.reply(`start watching Deni's status...`);
+    lastReportedDeniStatus = '';
+    clearTimeout(deniStatusWatchTimerId);
+    watchDeniStatus(ctx).catch(console.error);
+});
+
+bot.command('stop', async (ctx) => {
+    console.log(`Running stop command`);
+    ctx.reply(`stop watching Deni's status.`);
+    lastReportedDeniStatus = '';
+    clearTimeout(deniStatusWatchTimerId);
+});
+
+bot.command('deni', async (ctx) => {
+    const AVDIJA_COMMANDS =
+`/status - reports Deni Avdija's status.
+/start - start watching Deni Avdija's status.
+/stop - stop watching Deni Avdija's status.`;
+    ctx.reply(AVDIJA_COMMANDS);
 });
 
 async function init() {
     console.log(`Bot initializing...`);
     try {
-        await postMessage(AVAILABLE_COMMANDS);
+        await postMessage(ROOT_COMMANDS);
         await bot.launch();
         process.once('SIGINT', () => bot.stop('SIGINT'));
         process.once('SIGTERM', () => bot.stop('SIGTERM'));
